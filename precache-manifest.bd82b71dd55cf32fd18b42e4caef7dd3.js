fe8392fd92ea9ef960d9699ce347328d28b6f2be
pranshu2610 <b218030@iiit-bh.ac.in>
2020-07-06 17:19:44 +0530

    Updates

---
+++ precache-manifest.bd82b71dd55cf32fd18b42e4caef7dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c931ab4e48111cb0446f9e395d6aeb",
+    "url": "/stylish-form/index.html"
+  },
+  {
+    "revision": "b3acd3a461f04022ccbb",
+    "url": "/stylish-form/static/css/main.d91e237e.chunk.css"
+  },
+  {
+    "revision": "acf958e2d0384da03b15",
+    "url": "/stylish-form/static/js/2.07e943dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stylish-form/static/js/2.07e943dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3acd3a461f04022ccbb",
+    "url": "/stylish-form/static/js/main.f3003c94.chunk.js"
+  },
+  {
+    "revision": "b57a092d763abf612f28",
+    "url": "/stylish-form/static/js/runtime-main.166607d5.js"
+  }
+]);
